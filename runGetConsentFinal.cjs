06d6fe748946a16be03f6a2d10a0c9b29fe2cc5f
Yuri Almeida <73500564+yalmeidarj@users.noreply.github.com>
2024-02-21 04:53:14 -0500

    get consent final script

--- runGetConsentFinal.cjs
+++ runGetConsentFinal.cjs
@@ -1,10 +1,10 @@
-// runBot.cjs
-const bot = require("./bot.cjs");
+// runGetConsentFinal.cjs
+const bot = require("./getConsentFinal.cjs");
 
-  const chosenSite = "PCNGON62_1063A";
-  const username = "yalmeida.rj@gmail.com";
+const chosenSite = "PCNGON62_1063A";
+const username = "yalmeida.rj@gmail.com";
 const password = "rYeEsydWN!8808168eXkA9gV47A";
-  
+
 bot(chosenSite, username, password)
   .then((result) => console.log(result))
   .catch((error) => console.error(error));
